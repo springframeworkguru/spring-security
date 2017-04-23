4c056eea60d89ddb27dea8257fdb0708b25feae5
Armen G <gharibi.armen@yahoo.com>
2017-04-23 07:46:43 -0700

    Added partial passwordEncoder implemetation.

--- src/main/java/com/agharibi/config/SpringSecurityConfig.java
+++ src/main/java/com/agharibi/config/SpringSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.agharibi.config;
 
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,5 +13,23 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor) {
+
+
+        PasswordEncoder encoder = new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return null;
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return false;
+            }
+        }
+
     }
 }
