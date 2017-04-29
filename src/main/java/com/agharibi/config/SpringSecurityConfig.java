40fc8da647022d1cfbe1771fcf503ff441a74238
Armen G <gharibi.armen@yahoo.com>
2017-04-29 07:09:57 -0700

    spring security config passwordEncoder and authenicationProvider

--- src/main/java/com/agharibi/config/SpringSecurityConfig.java
+++ src/main/java/com/agharibi/config/SpringSecurityConfig.java
@@ -3,11 +3,16 @@ package com.agharibi.config;
 import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -17,19 +22,16 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor) {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        encoder.encode(passwordEncryptor.toString());
 
-
-        PasswordEncoder encoder = new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence charSequence) {
-                return null;
-            }
-
-            @Override
-            public boolean matches(CharSequence charSequence, String s) {
-                return false;
-            }
-        }
-
+        return encoder;
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder,
+                                                               UserDetailsService userDetailsService) {
+        return null;
+    }
+
 }
